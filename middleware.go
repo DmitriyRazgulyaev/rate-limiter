ebee231155e5a84cd86a841cd280a8068ce64535
dmitriyrazgulyaev <dimaraz2006@gmail.com>
2026-08-27 22:28:01 +0300

    add function for ip extraction

--- middleware.go
+++ middleware.go
@@ -6,33 +6,63 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"strings"
 )
 
 type RateLimiter interface {
 	Check(ctx context.Context, ip string) (bool, error)
 }
 
-func Middleware(limiter RateLimiter) func(http.Handler) http.Handler {
-	return func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			ip, _, err := net.SplitHostPort(r.RemoteAddr)
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusNotAcceptable)
-			}
-			userIP := net.ParseIP(ip)
-			if userIP == nil {
-				//TODO: проверить правильность кода ошибки
-				http.Error(w, fmt.Sprintf("userip: %q is not IP:port", r.RemoteAddr), http.StatusBadRequest)
+func getIPFromRequest(r *http.Request, proxiesAmount int) (string, error) {
+	if proxiesAmount > 0 {
+		ips := r.Header.Get("X-Forwarded-For")
+
+		if ips != "" {
+			addresses := strings.Split(ips, ",")
+
+			if len(addresses) < proxiesAmount {
+				return "", fmt.Errorf(
+					"invalid X-Forwarded-For: got %d addresses, expected at least %d",
+					len(addresses),
+					proxiesAmount,
+				)
 			}
 
-			isAllow, err := limiter.Check(context.Background(), userIP.String())
+			ip := strings.TrimSpace(addresses[len(addresses)-proxiesAmount])
+			parsedIP := net.ParseIP(ip)
+			if parsedIP == nil {
+				return "", fmt.Errorf("invalid IP in X-Forwarded-For: %q", ip)
+			}
+
+			return parsedIP.String(), nil
+		}
+	}
+
+	host, _, err := net.SplitHostPort(r.RemoteAddr)
+	if err != nil {
+		return "", fmt.Errorf("invalid RemoteAddr: %w", err)
+	}
+
+	ip := net.ParseIP(host)
+	if ip == nil {
+		return "", fmt.Errorf("invalid IP in RemoteAddr: %q", host)
+	}
+
+	return ip.String(), nil
+}
+
+func Middleware(limiter RateLimiter, proxiesAmount int) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			userIP, err := getIPFromRequest(r, proxiesAmount)
+
+			allowed, err := limiter.Check(r.Context(), userIP)
 			if err != nil {
-				log.Println(fmt.Sprintf("internal error: %w", err))
-				//TODO: проверить правильность ошибки
-				http.Error(w, fmt.Sprintf("internal error: %w", err), http.StatusInternalServerError)
+				log.Printf("internal error: %w", err)
+				http.Error(w, "internal error: %w", http.StatusInternalServerError)
 				return
 			}
-			if !isAllow {
+			if !allowed {
 				http.Error(w, "too many request", http.StatusTooManyRequests)
 				return
 			}
